059c7b325ce8f8134572bca2dab717d119d31c1d
Stefan Babic <babic.stefan@gmail.com>
2016-07-28 01:51:10 +0200

    update key

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,7 +4,7 @@ class Recipe
   default_options.update(verify: false)
 
   base_uri 'http://food2fork.com/api/search'
-  default_params key: ENV['FOOD2FORK_KEY'], q: "search"
+  default_params key: "7fc275b8a1193b722d960953615b242d", q: "search"
   format :json
 
   def self.for term
